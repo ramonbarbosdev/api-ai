303ef886f3e8a7dde9c79e5299b0409baa132d5d
Ramon Barbosa <ramon.barbosa@w5i.com.br>
2026-02-14 10:31:51 -0300

    novo commit

---
+++ src/main/java/com/controller/ChatController.java
@@ -0,0 +1,30 @@
+package com.controller;
+
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.*;
+import javax.validation.constraints.NotNull;
+
+@RestController
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+    public ChatController(@NotNull ChatClient.Builder chatClientBuilder)
+    {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping("/ai")
+    String generation(String userInput)
+    {
+        return  this.chatClient.prompt()
+                .user(userInput)
+                .call()
+                .content();
+    }
+}
